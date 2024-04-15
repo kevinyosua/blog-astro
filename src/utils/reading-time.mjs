9b2968d09453c0ed40ab142342a9a2d5d6f4de95
kevinyosua <kevinyl18@gmail.com>
2024-04-15 20:42:05 +0700

    fix blogpost layout

--- src/utils/reading-time.mjs
+++ src/utils/reading-time.mjs
@@ -2,11 +2,12 @@ import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 
 export function remarkReadingTime() {
-  return function(tree, { data }) {
+  return function(tree, props) {
+    if (!props?.data) return;
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
     // readingTime.text will give us minutes read as a friendly string,
     // i.e. "3 min read"
-    data.astro.frontmatter.minutesRead = readingTime.text;
+    props.data.astro.frontmatter.minutesRead = readingTime.text;
   };
 }
